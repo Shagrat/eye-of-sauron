d14164bb1f1dce00daa52066aab44b72574e605e
Travis CI <nobody@nobody.org>
2019-02-04 15:58:44 +0200

    Update docs

--- sleepless/swagger_export.py
+++ sleepless/swagger_export.py
@@ -1,9 +1,9 @@
 import os
 from flask import json
 from api.restplus import api
-from app import wsgi
+from wsgi import app
 
 
-with wsgi.test_request_context():
+with app.test_request_context():
     with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..', 'docs', 'swagger.json'), 'w') as stream:
          stream.write(json.dumps(api.__schema__))
