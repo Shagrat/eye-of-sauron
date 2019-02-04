1e639b92490c269c9b393341a53e50a00ebe90e9
Travis CI <nobody@nobody.org>
2019-02-04 16:06:53 +0200

    docs removed

--- scripts/update-pages-docs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -o errexit
-git config --global user.email "nobody@nobody.org"
-git config --global user.name "Travis CI"
-python sleepless/swagger_export.py
-cd docs
-git add swagger.json
-git commit -m "Update docs"
-git push --force --quiet "https://${GITHUB_TOKEN}@$github.com/${GITHUB_REPO}.git" master > /dev/null 2>&1
