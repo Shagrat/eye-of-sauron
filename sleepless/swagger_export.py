a88ea1e7c251102097c0bedd4eeaa26e1312c9e6
Sergey Funin <shagrat2006@gmail.com>
2019-02-04 15:59:00 +0200

    Fix

--- sleepless/swagger_export.py
+++ sleepless/swagger_export.py
@@ -1,9 +1,9 @@
 import os
 from flask import json
-from api.restplus import api
-from wsgi import app
+from app.api.restplus import api
+from app.wsgi import app
 
 
 with app.test_request_context():
-    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..', 'docs', 'swagger.json'), 'w') as stream:
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'docs', 'swagger.json'), 'w') as stream:
          stream.write(json.dumps(api.__schema__))
